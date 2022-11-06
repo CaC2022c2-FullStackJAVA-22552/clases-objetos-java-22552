c12ffdb78357ff25ffa9980431ee0c4f9e31a17f
Charly Cimino <carlosecimino@gmail.com>
2022-11-06 16:54:17 -0300

    Listo

---
+++ src/clasesobjetos/Auto.java
@@ -0,0 +1,15 @@
+
+package clasesobjetos;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Auto {
+    String marca;
+    String modelo;
+    String patente;
+    Motor motor;
+}
